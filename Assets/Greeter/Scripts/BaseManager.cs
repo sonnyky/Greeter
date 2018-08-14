df5f2663b3f8c4025dd231e7dd09682f47cc59d8
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-08-14 12:29:15 +0900

    added packman

---
+++ Assets/Greeter/Scripts/BaseManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BaseManager : MonoBehaviour {
+
+    public GameObject[] m_Managers;
+
+    public string m_NextSceneName = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Finish()
+    {
+        SceneManager.LoadScene(m_NextSceneName);
+    }
+}
