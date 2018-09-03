a82318d8be74e662ec185253b50933df6e20651e
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-09-03 19:11:30 +0900

    working sample

--- Assets/Greeter/Scripts/SpeechToText.cs
+++ Assets/Greeter/Scripts/SpeechToText.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 
 public class SpeechToText : MonoBehaviour {
 
     DictationRecognizer dictationRecognizer;
-
-
+    Text m_UserInput;
+    ConversationManager m_ConversationManager;
     // Use this for initialization
     void Start()
     {
+
+        m_ConversationManager = GameObject.Find("ConversationManager").GetComponent<ConversationManager>();
+        m_UserInput = GameObject.Find("Canvas").transform.Find("UserInput").transform.Find("Text Area").transform.Find("Text").GetComponent<Text>();
         foreach (string device in Microphone.devices)
         {
             Debug.Log("Name: " + device);
@@ -18,8 +22,8 @@ public class SpeechToText : MonoBehaviour {
 
         dictationRecognizer = new DictationRecognizer();
 
-        dictationRecognizer.AutoSilenceTimeoutSeconds = 5f;
-        dictationRecognizer.InitialSilenceTimeoutSeconds = 0f; 
+        //dictationRecognizer.AutoSilenceTimeoutSeconds = 5f;
+        //dictationRecognizer.InitialSilenceTimeoutSeconds = 0f; 
 
         dictationRecognizer.DictationResult += onDictationResult;
         dictationRecognizer.DictationHypothesis += onDictationHypothesis;
@@ -33,6 +37,7 @@ public class SpeechToText : MonoBehaviour {
     {
         // write your logic here
         Debug.LogFormat("Dictation result: " + text);
+        m_ConversationManager.SetUserInputFromVoice(text);
     }
 
     void onDictationHypothesis(string text)
